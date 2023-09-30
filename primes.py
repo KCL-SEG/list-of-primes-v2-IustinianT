02dc13f90481d370e8c8d47b4dcedb4007e3ab42
Iusti T <iustilaurentiu@yahoo.ro>
2023-09-30 21:24:48 +0100

    Complete exercise
    Created 'is_Prime' function to check if a number is prime, created a
    ValueError raise for if the value is 0 or lower, finished functionality
    of 'primes'.

--- primes.py
+++ primes.py
@@ -1,6 +1,29 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_Prime(num):
+    if num == 1 or num == 2:
+        return True
+    elif num == 4:
+        return False
+    
+    for i in range(2, num//2):
+        if num%i == 0:
+            return False
+    return True
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Value input is less than or equal to 0.")
+
     list = []
+
+    current_val = 2
+    while number_of_primes > len(list):
+        if is_Prime(current_val):
+            list.append(current_val)
+        current_val +=1
+
     return list
+
+print(primes(-2))
